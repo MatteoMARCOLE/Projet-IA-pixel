5462f15e514d9b8e2b461cf7d02801bb18ac8dcc
Maxime MILOIKOVITCH <maxime.miloikovitch@gmail.com>
2026-06-12 11:37:55 +0200

    ajout javadoc biome et clusteringInterface

--- Biome.java
+++ Biome.java
@@ -1,7 +1,17 @@
+/**
+ * Classe représentant un biome, avec son nom et sa couleur (r, g, b).
+ */
 public class Biome {
     public String nom;
     public int r, g, b;
 
+    /**
+     * Constructeur de la classe Biome
+     * @param nom le nom du biome
+     * @param r la composante rouge de la couleur du biome (entre 0 et 255)
+     * @param g la composante verte de la couleur du biome (entre 0 et 255)
+     * @param b la composante bleue de la couleur du biome (entre 0 et 255)
+     */
     public Biome(String nom, int r, int g, int b) {
         this.nom = nom;
         this.r = r;
